2bfbd06dc4b89dcd9d27cc6df5605fed13a1f788
aliflikescoding <alif.likes.coding@gmail.com>
2024-04-06 15:37:16 +0700

    Add data js file

---
+++ app/src/data.js
@@ -0,0 +1,58 @@
+export const initialUserInfo = {
+  name: "John Doe",
+  headerSection: {
+    0: {
+      headerId: 0,
+      headerTitle: "Jakarta, Indonesia",
+      headerLink: "",
+    },
+
+    1: {
+      headerId: 1,
+      headerTitle: "Github",
+      headerLink: "https://github.com",
+    },
+    2: {
+      headerId: 2,
+      headerTitle: "+62 1234567",
+      headerLink: "",
+    },
+  },
+  bodySection: {
+    0: {
+      bodyId: 0,
+      bodyTitle: "work experience & projects",
+      bodyPoints: {
+        0: {
+          pointId: 0,
+          pointTitle: "Project 1",
+          pointContent: "something",
+        },
+
+        1: {
+          pointId: 1,
+          pointTitle: "Project 2",
+          pointContent: "something",
+        },
+      },
+    },
+
+    1: {
+      bodyId: 1,
+      bodyTitle: "interests and hobbies",
+      bodyPoints: {
+        0: {
+          pointId: 0,
+          pointTitle: "Hobby 1",
+          pointContent: "something",
+        },
+
+        1: {
+          pointId: 1,
+          pointTitle: "Hobby 2",
+          pointContent: "something",
+        },
+      },
+    },
+  },
+};
