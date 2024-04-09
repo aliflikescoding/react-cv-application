61f9c52c07cbd0a627cc7cbfc43c51762eedd074
aliflikescoding <alif.likes.coding@gmail.com>
2024-04-09 11:46:46 +0700

    make body title class

---
+++ app/src/components/BodyTitle.jsx
@@ -0,0 +1,18 @@
+import "./BodyTitle.scss";
+import "../sass/fonts.scss";
+
+function BodyTitle({ bodyTitle, onChangeBodyTitle }) {
+  return (
+    <div className="input font-ubuntu">
+      <span className="input-label">Title: </span>
+      <input
+        className="input-text"
+        type="text"
+        value={bodyTitle}
+        onChange={onChangeBodyTitle}
+      />
+    </div>
+  );
+}
+
+export default BodyTitle;
