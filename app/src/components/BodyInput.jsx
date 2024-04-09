7fd9c3a0f86cef0b522f41e70f3fa89b84fbad57
aliflikescoding <alif.likes.coding@gmail.com>
2024-04-09 13:12:14 +0700

    Change prop name

--- app/src/components/BodyInput.jsx
+++ app/src/components/BodyInput.jsx
@@ -1,7 +1,7 @@
 import "./BodyInput.scss";
 import "../sass/fonts.scss";
 
-function BodyInput({ TitleInput, onChangeTitle, contentInput, onChangeInput }) {
+function BodyInput({ TitleInput, onChangeTitle, contentPoint, onChangePoint }) {
   return (
     <div className="input font-ubuntu">
       <span className="input-label">Title: </span>
@@ -14,8 +14,8 @@ function BodyInput({ TitleInput, onChangeTitle, contentInput, onChangeInput }) {
       <span className="input-label">Content: </span>
       <textarea
         className="input-text"
-        value={contentInput}
-        onChange={onChangeInput}
+        value={contentPoint}
+        onChange={onChangePoint}
         cols="30"
         rows="10"
       ></textarea>
