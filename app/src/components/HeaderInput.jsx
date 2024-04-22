14de62d83689106125d7fa92c52465894af31d59
aliflikescoding <alif.likes.coding@gmail.com>
2024-04-22 22:02:04 +0700

    Styled header input

--- app/src/components/HeaderInput.jsx
+++ app/src/components/HeaderInput.jsx
@@ -1,7 +1,14 @@
 import "./HeaderInput.scss";
 import "../sass/fonts.scss";
+import "../sass/components.scss";
 
-function HeaderInput({ TitleInput, onChangeTitle, linkInput, onChangeLink, onDeleteSection }) {
+function HeaderInput({
+  TitleInput,
+  onChangeTitle,
+  linkInput,
+  onChangeLink,
+  onDeleteSection,
+}) {
   return (
     <div className="input font-ubuntu">
       <span className="input-label">Title: </span>
@@ -18,7 +25,12 @@ function HeaderInput({ TitleInput, onChangeTitle, linkInput, onChangeLink, onDel
         value={linkInput}
         onChange={onChangeLink}
       />
-      <button className="header-delete-button" onClick={onDeleteSection}>x</button>
+      <button
+        className="header-delete-button buttonEmpty buttonEmpty-cross"
+        onClick={onDeleteSection}
+      >
+        x
+      </button>
     </div>
   );
 }
